e2c5f484c86cde0ca05b17bb944abea6cbdea9d1
Sammi Turner <ms.sammi.turner@gmail.com>
2024-10-26 21:36:40 +0100

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-blue-500 text-white">
+      <h1 className="text-center text-2xl font-bold">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
